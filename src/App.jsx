1f5fa3be2128553cfa4206d6a80c9d5531f8da3a
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-02-19 21:27:22 +0100

    working on Add a new Project component + validation for form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import NewProject from "./components/NewProject/newproject";
+import NoProjectSelected from "./components/NoProjectSelected/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Navbar />
-      <NewProject />
+      <Navbar onStartAddProject={handleStartAddProject} />
+      {content};
     </main>
   );
 }
