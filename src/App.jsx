a7c333a6ae6b8ab3b4f415162c544d80acdfbf90
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-02-27 14:50:45 +0100

    Add deletion of projects

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     projects: [],
   });
 
+  //Sidebar function
   function handleSelectProject(id) {
     setProjectState((prevState) => {
       return {
@@ -19,6 +20,7 @@ function App() {
     });
   }
 
+  //Page to add Project function
   function handleStartAddProject() {
     setProjectState((prevState) => {
       return {
@@ -27,6 +29,8 @@ function App() {
       };
     });
   }
+
+  //Cancel add function
   function handleCancelAddProject() {
     setProjectState((prevState) => {
       return {
@@ -35,6 +39,8 @@ function App() {
       };
     });
   }
+
+  //Add a project function
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
       const newProject = {
@@ -51,11 +57,25 @@ function App() {
     });
   }
 
+  //Deleting a project function
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
